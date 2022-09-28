dbbb9172491ccbf3fa65d47d32a6d937be5c2fb7
高飞宇 <gaofeiyu01@58.com>
2022-09-28 23:20:42 +0800

    test: add vitest

--- packages/whimsical/src/App.tsx
+++ packages/whimsical/src/App.tsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const onMouseEnter = () => {
+    setCount(1);
+  };
+
+  const onMouseLeave = () => {
+    setCount(0);
+  };
 
   return (
-    <div className="App">
+    <div
+      className={count === 0 ? 'App' : ''}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -17,18 +29,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ packages/whimsical/src/__tests__/index.spec.tsx
@@ -0,0 +1,29 @@
+import { test, expect } from 'vitest';
+import renderer from 'react-test-renderer';
+import App from '../App';
+
+function toJson(component: renderer.ReactTestRenderer) {
+  const result = component.toJSON();
+  expect(result).toBeDefined();
+  expect(result).not.toBeInstanceOf(Array);
+  return result as renderer.ReactTestRendererJSON;
+}
+
+test('App changes the class when hovered', () => {
+  const component = renderer.create(<App />);
+  let tree: renderer.ReactTestRendererJSON = toJson(component);
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  tree.props.onMouseEnter();
+
+  // re-rendering
+  tree = toJson(component);
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  tree.props.onMouseLeave();
+  // re-rendering
+  tree = toJson(component);
+  expect(tree).toMatchSnapshot();
+});
